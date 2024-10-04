32c1d4c7468fb92873dd3b37c7b4ea1069480fb7
CarbonicSoda <carbonsoda@outlook.com>
2024-10-04 22:51:30 +0800

    completed the code

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,166 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+let statusBarItem: vscode.StatusBarItem;
+let active: boolean;
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "istrainless" is now active!');
+export async function activate() {
+	active = true;
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('istrainless.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Istrainless!');
+	vscode.commands.registerCommand("istrainless.setBreakTimeout", async () => {
+		const input = await vscode.window.showInputBox({
+			title: "IstrainLess Break Timeout",
+			prompt: "Enter New Break Timeout in Minutes",
+			validateInput: (input) => {
+				const num = parseFloat(input);
+				if (isNaN(num)) return "Invalid Timeout";
+				if (num < 1) return "Timeout Too Short";
+			},
+		});
+		if (!input) return;
+		const config = vscode.workspace.getConfiguration("istrainless");
+		config.update("breakTimeout", parseFloat(input), true);
+	});
+	vscode.commands.registerCommand("istrainless.setBreakDuration", async () => {
+		const input = await vscode.window.showInputBox({
+			title: "IstrainLess Break Duration",
+			prompt: "Enter New Break Duration in Minutes",
+			validateInput: (input) => {
+				const num = parseFloat(input);
+				if (isNaN(num)) return "Invalid Duration";
+				if (num < 0.25) return "Duration Too Short";
+			},
+		});
+		if (!input) return;
+		const config = vscode.workspace.getConfiguration("istrainless");
+		config.update("breakDuration", parseFloat(input), true);
 	});
 
-	context.subscriptions.push(disposable);
+	statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 0);
+	statusBarItem.name = "IstrainLess";
+	statusBarItem.show();
+
+	mainLoop();
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+async function mainLoop() {
+	while (active) {
+		const config = vscode.workspace.getConfiguration("istrainless");
+		const breakTimeout: number = config.get("breakTimeout");
+		const breakDuration: number = config.get("breakDuration");
+
+		let timeLeft = breakTimeout * 60;
+		const updateStatusBarItem = async () => {
+			let time = await getTime(timeLeft--);
+			statusBarItem.text = `$(watch) ${time}`;
+			statusBarItem.tooltip = `${time} Until Next Break`;
+			statusBarItem.backgroundColor =
+				timeLeft < 60 ? new vscode.ThemeColor("statusBarItem.warningBackground") : null;
+		};
+		statusBarItem.color = null;
+		await updateStatusBarItem();
+		const timeElapse = setInterval(updateStatusBarItem, 1e3);
+
+		await sleep(breakTimeout * 6e4);
+		clearInterval(timeElapse);
+		statusBarItem.text = statusBarItem.tooltip = "On Break";
+		statusBarItem.color = new vscode.ThemeColor("statusBarItem.warningForeground");
+		statusBarItem.backgroundColor = null;
+
+		await startBreakSession(breakDuration);
+	}
+}
+
+async function startBreakSession(duration: number) {
+	const quotes = [
+		"Peek Outside the Window for Some Shaft of Light!",
+		"Take the Time to Do Some Stretching~",
+		"Good Time to Wash Your Face!",
+		"Touch Some Grass!",
+	];
+
+	const panel = vscode.window.createWebviewPanel("istrainless.break", "IstrainLess", vscode.ViewColumn.One, {
+		enableScripts: true,
+		retainContextWhenHidden: true,
+	});
+	panel.webview.html = `<!DOCTYPE html>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>IstrainLess</title>
+		<style>
+			html, body {
+				height: 100%;
+				width: auto;
+			}
+			#main {
+				height: 100%;
+				width: 100%;
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				align-items: center;
+			}
+			#quote {
+				font-family: var(--vscode-editor-font-family);
+				font-weight: var(--vscode-editor-font-weight);
+				font-size: calc(var(--vscode-editor-font-size) * 1.3);
+				color: var(--vscode-editor-foreground);
+			}
+			@keyframes timeout {
+				from {width: 80%;}
+				to {width: 0;}
+			}
+			#timeout-bar {
+				height: 4%;
+				border-radius: 3px;
+				background-color: var(--vscode-editor-foreground);
+				animation: timeout ${duration * 60}s linear;
+			}
+			#timeout {
+				font-family: var(--vscode-editor-font-family);
+				font-weight: var(--vscode-editor-font-weight);
+				font-size: var(--vscode-editor-font-size);
+				color: var(--vscode-editor-foreground);
+			}
+		</style>
+		<script>
+			${getTime}
+		</script>
+	</head>
+	<body>
+		<div id="main">
+			<h1 id="quote">${quotes[Math.trunc(quotes.length * Math.random())]}</h1>
+			<div id="timeout-bar"></div>
+			<p id="timeout">--:--</p>
+		</div>
+	</body>
+	<script>
+		let timeLeft = ${Math.round(duration * 60)};
+		const timeout = document.getElementById("timeout");
+		const countdown = setInterval(async () => timeout.textContent = await getTime(timeLeft--), 1e3);
+		setTimeout(async () => clearInterval(countdown), ${duration * 6e4});
+		(async () => timeout.textContent = await getTime(timeLeft--))();
+	</script>
+	</html>`;
+
+	const forceReveal = panel.onDidChangeViewState(async () => panel.reveal(vscode.ViewColumn.One));
+	await sleep(duration * 6e4);
+	forceReveal.dispose();
+	panel.dispose();
+}
+
+export async function deactivate() {
+	active = false;
+	statusBarItem?.dispose();
+}
+
+async function sleep(ms: number) {
+	return await new Promise((res) => setTimeout(res, ms));
+}
+
+async function getTime(sec: number) {
+	const m = Math.trunc(sec / 60);
+	const s = Math.trunc(sec - m * 60);
+	return String(m).padStart(2, "0") + ":" + String(s).padStart(2, "0");
+}
